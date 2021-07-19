a9c9987db8edc04bf781c087e6d07c8a113009de
vsuvardhanreddy <vsuvardhanreddy@gmail.com>
2021-07-19 18:15:07 +0530

    these are for learning

---
+++ Describe.java
@@ -0,0 +1,11 @@
+Public Class Describe{
+
+    public void describeWorld(){
+        System.out.println("What a wonderful world");
+    }
+
+    public static void main(String args[]){
+        Describe hi = new Describe();
+        hi.describeWorld();
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,11 @@
+Public Class Hello{
+
+    public void sayHello(){
+        System.out.println("Hello to the world);
+    }
+
+    public static void main(String args[]){
+        Hello hi = new Hello();
+        hi.sayHello();
+    }
+}
